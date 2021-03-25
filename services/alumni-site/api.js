f6dee4c68c2c4a9c801c4ebf0db8dcca3f8bc20e
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-03-25 13:10:25 -0300

    refactoring on api.js.

--- services/alumni-site/api.js
+++ services/alumni-site/api.js
@@ -1,24 +1,9 @@
 import axios from "axios";
 
-
 // Starting points
 const START_POINT = "";
 const EURECA_AS = `${START_POINT}:8080/`;
-const EURECA_BACKEND = `${START_POINT}:8081/`;
 const ALUMNI_AS = `${START_POINT}:8082/`;
-
-// Default for tests
-const api = axios.create({
-  baseURL: EURECA_BACKEND,
-});
-
-// for Eureca Backend
-const api_EB = axios.create({
-  baseURL: EURECA_BACKEND,
-  headers: {
-    "Content-type": "application/json",
-  },
-});
 
 // for Eureca Authentication Service
 const api_EAS = axios.create({
@@ -36,5 +21,4 @@ const api_AS = axios.create({
   },
 });
 
-export default api;
 export { api_EAS, api_AS };
