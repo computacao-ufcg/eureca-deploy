176562072bec8827128443494d8ca2e2d4ca18c2
fubica <fubica@dsc.ufcg.edu.br>
2021-06-01 11:27:35 -0300

    Bug fix

--- scripts/deploy-and-start-services.sh
+++ scripts/deploy-and-start-services.sh
@@ -1,129 +1,13 @@
 #!/bin/bash
 
-BUILD_FILE_NAME="build"
-WORK_DIR=$(pwd)
-SERVICE_CONF_FILE_PATH="./conf-files/service.conf"
+./scripts/start-eureca-as.sh
 
-# Read configuration file
+./scripts/start-eureca-backend.sh
 
-AS_PORT_PATTERN="as_port"
-AS_PORT=$(grep $AS_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-ALUMNI_PORT_PATTERN="alumni_port"
-ALUMNI_PORT=$(grep $ALUMNI_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-FRONTEND_PORT_PATTERN="frontend_port"
-FRONTEND_PORT=$(grep $FRONTEND_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-BACKEND_PORT_PATTERN="backend_port"
-BACKEND_PORT=$(grep $BACKEND_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-ALUMNI_SITE_PORT_PATTERN="alumni_site_port"
-ALUMNI_SITE_PORT=$(grep $ALUMNI_SITE_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-HTTP_PORT_PATTERN="http_port"
-HTTP_PORT=$(grep $HTTP_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-HTTPS_PORT_PATTERN="https_port"
-HTTPS_PORT=$(grep $HTTPS_PORT_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
+./scripts/start-apache.sh
 
-AS_TAG_PATTERN="as_tag"
-AS_TAG=$(grep $AS_TAG_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-if [ -z ${AS_TAG// } ]; then
-	AS_TAG="latest"
-fi
-ALUMNI_TAG_PATTERN="alumni_tag"
-ALUMNI_TAG=$(grep $ALUMNI_TAG_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-if [ -z ${ALUMNI_TAG// } ]; then
-	ALUMNI_TAG="latest"
-fi
-FRONTEND_TAG_PATTERN="frontend_tag"
-FRONTEND_TAG=$(grep $FRONTEND_TAG_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-if [ -z ${FRONTEND_TAG// } ]; then
-	FRONTEND_TAG="latest"
-fi
-BACKEND_TAG_PATTERN="backend_tag"
-BACKEND_TAG=$(grep $BACKEND_TAG_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-if [ -z ${BACKEND_TAG// } ]; then
-	BACKEND_TAG="latest"
-fi
-ALUMNI_SITE_TAG_PATTERN="alumni_site_tag"
-ALUMNI_SITE_TAG=$(grep $ALUMNI_SITE_TAG_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-if [ -z ${ALUMNI_SITE_TAG// } ]; then
-	ALUMNI_SITE_TAG="latest"
-fi
-APACHE_TAG_PATTERN="apache_tag"
-APACHE_TAG=$(grep $APACHE_TAG_PATTERN $SERVICE_CONF_FILE_PATH | cut -d"=" -f2-)
-if [ -z ${APACHE_TAG// } ]; then
-	APACHE_TAG="latest"
-fi
+./scripts/start-alumni-site.sh
 
-echo "Removing containers"
-sudo docker stop eureca-frontend alumni-site eureca-as eureca-backend alumni-backend apache
-sudo docker rm eureca-frontend alumni-site eureca-as eureca-backend alumni-backend apache
+./scripts/start-eureca-frontend.sh
 
-echo "Creating containers"
-
-# Start Eureca Frontend
-sudo docker pull eureca/eureca-frontend:$FRONTEND_TAG
-sudo docker run -itd --name eureca-frontend \
-    -p $FRONTEND_PORT:3000 \
-    -v $WORK_DIR/conf-files/frontend/api.js:/root/eureca-frontend/services/api.js \
-    eureca/eureca-frontend:$FRONTEND_TAG
-
-# Start Alumni Site
-sudo docker pull eureca/alumni-site:$ALUMNI_SITE_TAG
-sudo docker run -itd --name alumni-site \
-    -p $ALUMNI_SITE_PORT:3001 \
-    -v $WORK_DIR/conf-files/alumni-site/api.js:/root/alumni-site/services/api.js \
-    -v $WORK_DIR/conf-files/alumni-site/login.js:/root/alumni-site/services/login.js \
-    eureca/alumni-site:$ALUMNI_SITE_TAG
-
-# Start Eureca AS
-
-sudo docker pull eureca/eureca-as:$AS_TAG
-sudo docker run -itd --name eureca-as \
-    -p $AS_PORT:8080 \
-    -v $WORK_DIR/conf-files/as:/root/eureca-as/src/main/resources/private \
-    eureca/eureca-as:$AS_TAG
-
-# Start Eureca Backend
-sudo docker pull eureca/eureca-backend:$BACKEND_TAG
-sudo docker run -itd --name eureca-backend \
-    -p $BACKEND_PORT:8081 \
-    -v $WORK_DIR/conf-files/backend:/root/eureca-backend/src/main/resources/private \
-    eureca/eureca-backend:$BACKEND_TAG
-
-# Start Alumni Backend
-sudo docker pull eureca/alumni-backend:$ALUMNI_TAG
-sudo docker run -itd --name alumni-backend \
-    -p $ALUMNI_PORT:8082 \
-    -v $WORK_DIR/conf-files/alumni:/root/alumni-backend/src/main/resources/private \
-    eureca/alumni-backend:$ALUMNI_TAG
-
-# Start Apache
-sudo docker pull fogbow/apache-shibboleth-server:$APACHE_TAG
-sudo docker run -tdi --name apache \
-      -p $HTTP_PORT:80 \
-      -p $HTTPS_PORT:443 \
-      -v $WORK_DIR/conf-files/apache/site.crt:/etc/ssl/certs/site.crt \
-      -v $WORK_DIR/conf-files/apache/site.key:/etc/ssl/private/site.key \
-      -v $WORK_DIR/conf-files/apache/site.pem:/etc/ssl/certs/site.pem \
-      -v $WORK_DIR/conf-files/apache/ports.conf:/etc/apache2/ports.conf \
-      -v $WORK_DIR/conf-files/apache/000-default.conf:/etc/apache2/sites-available/000-default.conf \
-      fogbow/apache-shibboleth-server:$APACHE_TAG
-
-# Start Apache
-ENABLE_MODULES_SCRIPT="enable-modules"
-APACHE_CONTAINER_NAME="apache"
-
-echo "#!/bin/bash" > $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod ssl_load" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod proxy.load" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod proxy_http.load" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod shib2" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod ssl" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod rewrite" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod headers" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/a2enmod proxy_http" >> $ENABLE_MODULES_SCRIPT
-echo "/usr/sbin/service apache2 restart" >> $ENABLE_MODULES_SCRIPT
-
-sudo chmod +x $ENABLE_MODULES_SCRIPT
-sudo docker cp $ENABLE_MODULES_SCRIPT $APACHE_CONTAINER_NAME:/$ENABLE_MODULES_SCRIPT
-sudo docker exec $APACHE_CONTAINER_NAME /$ENABLE_MODULES_SCRIPT
-sudo docker exec $APACHE_CONTAINER_NAME /bin/bash -c "rm /$ENABLE_MODULES_SCRIPT"
-rm $ENABLE_MODULES_SCRIPT
+./scripts/start-alumni-backend.sh
